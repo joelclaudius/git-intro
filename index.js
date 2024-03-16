af8b3efeacf723689f8a3716883bde4f6935ee98
Claudius Joel <92925712+joelclaudius@users.noreply.github.com>
2024-03-16 23:40:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function upperCase() {
     const x = document.getElementById("fname");
     x.value = x.value.toUpperCase();
 }
-// function upperCase() {
-//     const x = document.getElementById("fname");
-//     x.value = x.value.toUpperCase();
-// }
+function upperCase() {
+    const x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
